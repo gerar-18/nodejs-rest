9463d8f70963acc92c8686fbc44d9ac05a61ff5f
Gerardo Lopez <gl815465@gmail.com>
2020-08-25 12:06:34 -0500

    Subiendo archivos

---
+++ src/index.js
@@ -0,0 +1,98 @@
+const http = require('http');
+const { bodyParser } = require('./lib/bodyParser');
+let database = [];
+function getTaskHandler(req, res){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(database));
+    res.end();
+}
+async function createTaskHandler(req, res){
+    try {
+        await bodyParser(req);
+        database.push(req.body);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(database));
+        res.end();
+    } catch (error) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Dato invalido ');
+        res.end();
+    }
+}
+async function updateTastkHandler(req, res){
+    try {
+        let {url} = req;
+        let idQuery = url.split("?")[1];
+        let idKey = idQuery.split("=")[0];
+        let idValue = idQuery.split("=")[1];
+        if(idKey === "id"){
+            await bodyParser(req);
+            database[idValue - 1] = req.body;
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(database));
+            res.end();
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('Request invalido');
+            res.end();
+        }
+    } catch (error) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write('Datos invalidos provistos ', error.message);
+        res.end();
+    }
+    
+    
+}
+async function deleteTaskHandler(req, res){
+    let {url} = req;
+    let idQuery = url.split("?")[1];
+    let idKey = idQuery.split("=")[0];
+    let idValue = idQuery.split("=")[1];
+    if(idKey === 'id'){
+        database.splice(idValue-1,1);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write("Delete satisfactorio");
+        res.end();
+    }else{
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write("Petición invalida");
+        res.end();
+    }
+}
+const server = http.createServer((req, res) => {
+    const { url, method } = req;
+    //Loger
+    console.log(`URL: ${url} - Method: ${method}`);
+
+    switch(method){
+        case "GET":
+            if (url === "/"){
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.write(JSON.stringify({message: 'Hello word'}));
+                res.end();
+            }
+            if(url === "/tasks"){
+                getTaskHandler(req, res);
+            }
+            break;
+        case "POST":
+            if(url === "/tasks"){
+                createTaskHandler(req, res);
+            }
+            break;
+        case "PUT":
+            updateTastkHandler(req,res);
+            break;
+        case "DELETE":
+            deleteTaskHandler(req,res);
+            break;
+        default:
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write("Peticion invalida");
+            res.end();
+
+    }
+})
+server.listen(3000);
+console.log('Servidor en puerto: ',3000);
